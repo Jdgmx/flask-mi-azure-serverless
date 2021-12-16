b21c73faf2e35ec4d0d4e618444f7b937fb14d6b
Joaquin Durand <Joakod@yahoo.com>
2021-12-16 16:52:37 +0000

    edited the variables

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,6 +25,6 @@
 		}
 	}'\
 		  -H "Content-Type: application/json" \
-		  -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+		  -X POST https://joakod-ml-service.azurewebsites.net:$PORT/predict 
 		  #your application name <yourappname>goes here
 		  
